1bbc8a425804a69c91968dc1380a6e468b1f71b4
赵 <zhaoze@harmonycloud.cn>
2025-09-30 10:35:44 +0800

    fix get dledger mod storepath

--- store/src/main/java/org/apache/rocketmq/store/DefaultMessageStore.java
+++ store/src/main/java/org/apache/rocketmq/store/DefaultMessageStore.java
@@ -2025,7 +2025,7 @@ public class DefaultMessageStore implements MessageStore {
         private boolean isSpaceToDelete() {
             cleanImmediately = false;
 
-            String commitLogStorePath = DefaultMessageStore.this.getMessageStoreConfig().getStorePathCommitLog();
+            String commitLogStorePath = DefaultMessageStore.this.getStorePathPhysic();
             String[] storePaths = commitLogStorePath.trim().split(MixAll.MULTI_PATH_SPLITTER);
             Set<String> fullStorePath = new HashSet<>();
             double minPhysicRatio = 100;
